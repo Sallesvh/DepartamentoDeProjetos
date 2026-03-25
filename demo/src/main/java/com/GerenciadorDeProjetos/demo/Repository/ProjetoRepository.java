0053a8c2c2fe9789430c66325742155c27c0b07a
Victor Salles <sallesvictorhugo119@gmail.com>
2026-03-24 22:10:30 -0300

    Comit Repository

--- demo/src/main/java/com/GerenciadorDeProjetos/demo/Repository/ProjetoRepository.java
+++ demo/src/main/java/com/GerenciadorDeProjetos/demo/Repository/ProjetoRepository.java
@@ -1,5 +1,5 @@
 package com.GerenciadorDeProjetos.demo.Repository;
-
+//Comit Repository
 import com.GerenciadorDeProjetos.demo.Model.ProjetoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
